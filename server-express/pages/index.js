e47e5c40b6814f1472574e5eef9997a2d3528d63
Vinh Pham <phamtanvinh.me@gmail.com>
2019-10-09 17:38:14 +0700

    demo node socket

--- server-express/pages/index.js
+++ server-express/pages/index.js
@@ -2,16 +2,16 @@ import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import io from 'socket.io-client'
 
-const socket = io('http://localhost:3500')
+const socket = io('http://localhost:3600')
 
 const Index = () => {
   const [data, setData] = useState(null)
 
-  socket.on('connect', () => {
-    socket.on('stream', data => {
-      setData(data)
+  useEffect(() => {
+    socket.on('getcounter', respone => {
+      setData(respone)
     })
-  })
+  }, [data])
 
   const handleOnClick = e => {
     socket.emit('click', 'Client message')
